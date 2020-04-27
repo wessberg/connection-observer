5a85d29d14fbf1b433bf2c5f4db8c665f6333943
Steffen Eckardt <seckardt@salesforce.com>
2020-04-27 15:51:18 +0200

    fix: fix issues due to NodeList iterations in consuming projects

--- src/util/next-microtask.ts
+++ src/util/next-microtask.ts
@@ -2,7 +2,7 @@
  * Enqueues the given function for the next microtask
  * @param {CallableFunction} func
  */
-export const nextMicrotask = (func: CallableFunction): void => {
+export const nextMicrotask = (func: VoidFunction): void => {
 	if (typeof queueMicrotask !== "undefined") queueMicrotask(func);
 	else if (typeof Promise !== "undefined") Promise.resolve().then(() => func());
 	else setTimeout(() => func(), 0);
